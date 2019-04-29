nestybox/sysbox-mgr

commit 4e5e8d9357e1c836bd2b6379be6ab7b3149d39e7

Added code to track subid allocations on a per container basis, using the container's ID.
This is useful to ensure coupling between subid allocations and freeing.

subidAlloc/subidAlloc.go
@@ -33,7 +33,6 @@ import (
 	intf "github.com/nestybox/sysvisor/sysvisor-mgr/intf"
 	"github.com/nestybox/sysvisor/sysvisor-mgr/lib/buddyAlloc"
 	"github.com/opencontainers/runc/libcontainer/user"
-	"github.com/sirupsen/logrus"
 )
 
 const (
@@ -48,12 +47,18 @@ const (
 	NoReuse                    // do not re-use (allocation fails with error = "exhausted")
 )
 
+type allocInfo struct {
+	uid uint32
+	gid uint32
+}
+
 // subidAlloc class (implements the UidAllocator interface)
 type subidAlloc struct {
-	subuids   []user.SubID        // subuid range(s)
-	subgids   []user.SubID        // subgid range(s)
-	uidAllocs []*buddyAlloc.Buddy // uid allocator(s) (one per contiguous subuid range)
-	gidAllocs []*buddyAlloc.Buddy // gid allocator(s) (one per contiguous subuid range)
+	subuids   []user.SubID         // subuid range(s)
+	subgids   []user.SubID         // subgid range(s)
+	uidAllocs []*buddyAlloc.Buddy  // uid allocator(s) (one per contiguous subuid range)
+	gidAllocs []*buddyAlloc.Buddy  // gid allocator(s) (one per contiguous subuid range)
+	allocMap  map[string]allocInfo // table of container Ids and associated uid/gid allocs
 }
 
 func toBuddyPolicy(p ReusePolicy) buddyAlloc.ReusePolicy {
@@ -108,6 +113,7 @@ func New(userName string, reuse ReusePolicy, subuidSrc, subgidSrc io.Reader) (in
 		subgids:   subgids,
 		uidAllocs: make([]*buddyAlloc.Buddy, len(subuids)),
 		gidAllocs: make([]*buddyAlloc.Buddy, len(subgids)),
+		allocMap:  make(map[string]allocInfo),
 	}
 
 	// for each subuid range that is large enough, create a buddy allocator
@@ -213,7 +219,7 @@ func (sub *subidAlloc) freeGid(gid uint32) error {
 }
 
 // Implements intf.SubidAlloc.Alloc
-func (sub *subidAlloc) Alloc(size uint64) (uint32, uint32, error) {
+func (sub *subidAlloc) Alloc(id string, size uint64) (uint32, uint32, error) {
 
 	uid, err := sub.allocUid(size)
 	if err != nil {
@@ -225,15 +231,20 @@ func (sub *subidAlloc) Alloc(size uint64) (uint32, uint32, error) {
 		return 0, 0, err
 	}
 
-	logrus.Debugf("Alloc(%v): uid = %v, gid = %v\n", size, uid, gid)
-
+	sub.allocMap[id] = allocInfo{uid, gid}
 	return uid, gid, nil
 }
 
 // Implements intf.SubidAlloc.Free
-func (sub *subidAlloc) Free(uid, gid uint32) error {
+func (sub *subidAlloc) Free(id string) error {
 
-	logrus.Debugf("Free(%v, %v)\n", uid, gid)
+	m, ok := sub.allocMap[id]
+	if !ok {
+		return fmt.Errorf("not-found")
+	}
+
+	uid := m.uid
+	gid := m.gid
 
 	err := sub.freeUid(uid)
 	if err != nil {

intf/intf.go
@@ -8,12 +8,13 @@ package intf
 // performs or subuid and subgid allocations
 type SubidAlloc interface {
 
-	// Allocates an unused range of 'size' uids and gids; possible errors are
-	// nil, "exhausted", or "invalid-size". Max supported size is 2^32.
-	Alloc(size uint64) (uint32, uint32, error)
+	// Allocates an unused range of 'size' uids and gids for the container with the given
+	// 'id'; possible errors are nil, "exhausted", or "invalid-size". Max supported 'size' is
+	// 2^32.
+	Alloc(id string, size uint64) (uint32, uint32, error)
 
-	// Free releases a previously allocated uid and gid range; the given uid and gid must
-	// be obtained from a previous successful call to Alloc(); possible errors are nil or
-	// "not-found" (if the given uid or gid is not from a prior call to Alloc())
-	Free(uid, gid uint32) error
+	// Free releases a previously allocated uid and gid range for the container with the
+	// given 'id'. Possible errors are nil and "not-found" (if the container with the
+	// given 'id' has no allocations).
+	Free(id string) error
 }

commit 3d3c838c5df377873aa8b47115c138a6ac585d38

Fix a couple of bugs in subidAlloc package.
This change fixes a couple of problems in the subidAlloc package,
introduced by 4e5e8d9357e1c836bd2b6379be6ab7b3149d39e7.

That commit broke all of the unit tests, and also had a couple of
bugs in the alloc and free implementations.

This change fixes all of the above.

subidAlloc/subidAlloc.go
@@ -221,6 +221,10 @@ func (sub *subidAlloc) freeGid(gid uint32) error {
 // Implements intf.SubidAlloc.Alloc
 func (sub *subidAlloc) Alloc(id string, size uint64) (uint32, uint32, error) {
 
+	if _, found := sub.allocMap[id]; found {
+		return 0, 0, fmt.Errorf("exhausted")
+	}
+
 	uid, err := sub.allocUid(size)
 	if err != nil {
 		return 0, 0, err
@@ -238,8 +242,8 @@ func (sub *subidAlloc) Alloc(id string, size uint64) (uint32, uint32, error) {
 // Implements intf.SubidAlloc.Free
 func (sub *subidAlloc) Free(id string) error {
 
-	m, ok := sub.allocMap[id]
-	if !ok {
+	m, found := sub.allocMap[id]
+	if !found {
 		return fmt.Errorf("not-found")
 	}
 
@@ -255,6 +259,8 @@ func (sub *subidAlloc) Free(id string) error {
 	if err != nil {
 		return err
 	}
+
+	delete(sub.allocMap, id)
 
 	return nil
 }

subidAlloc/subidAlloc_test.go
@@ -8,6 +8,7 @@ import (
 )
 
 type allocTest struct {
+	id      string
 	size    uint64
 	wantUid uint32
 	wantGid uint32
@@ -17,7 +18,7 @@ type allocTest struct {
 func testAlloc(t *testing.T, subidAlloc intf.SubidAlloc, tests []allocTest) {
 
 	for _, test := range tests {
-		gotUid, gotGid, gotErr := subidAlloc.Alloc(test.size)
+		gotUid, gotGid, gotErr := subidAlloc.Alloc(test.id, test.size)
 
 		var errStr string
 		if gotErr == nil {
@@ -35,8 +36,8 @@ func testAlloc(t *testing.T, subidAlloc intf.SubidAlloc, tests []allocTest) {
 				test.wantErr = "(no-error)"
 			}
 
-			t.Errorf("Alloc(%v) failed: got = %v,%v,%v; want = %v,%v,%v",
-				test.size, gotUid, gotGid, errStr, test.wantUid, test.wantGid, test.wantErr)
+			t.Errorf("Alloc(%v, %v) failed: got = %v,%v,%v; want = %v,%v,%v",
+				test.id, test.size, gotUid, gotGid, errStr, test.wantUid, test.wantGid, test.wantErr)
 		}
 	}
 }
@@ -53,11 +54,11 @@ func TestAllocBasic(t *testing.T) {
 	}
 
 	var tests = []allocTest{
-		// size, wantUid, wantGid, wantErr
-		{65536, 0, 0, ""},
-		{65536, 65536, 65536, ""},
-		{65536, (65536 * 2), (65536 * 2), ""},
-		{65536, (65536 * 3), (65536 * 3), ""},
+		// id, size, wantUid, wantGid, wantErr
+		{"1", 65536, 0, 0, ""},
+		{"2", 65536, 65536, 65536, ""},
+		{"3", 65536, (65536 * 2), (65536 * 2), ""},
+		{"4", 65536, (65536 * 3), (65536 * 3), ""},
 	}
 
 	testAlloc(t, subidAlloc, tests)
@@ -75,11 +76,11 @@ func TestAllocRangeLimit(t *testing.T) {
 	}
 
 	var tests = []allocTest{
-		// size, wantUid, wantGid, wantErr
-		{655361, 0, 0, "exhausted"}, // exceeds range by 1
-		{524289, 0, 0, "exhausted"}, // exceeds nominal range by 1
-		{0, 0, 0, "invalid-size"},
-		{524288, 0, 0, ""}, // matches nominal range
+		// id, size, wantUid, wantGid, wantErr
+		{"1", 655361, 0, 0, "exhausted"}, // exceeds range by 1
+		{"2", 524289, 0, 0, "exhausted"}, // exceeds nominal range by 1
+		{"3", 0, 0, 0, "invalid-size"},
+		{"4", 524288, 0, 0, ""}, // matches nominal range
 	}
 
 	testAlloc(t, subidAlloc, tests)
@@ -101,25 +102,27 @@ func TestAllocReuse(t *testing.T) {
 
 	// initial allocs
 	for i := 0; i < 2; i++ {
+		id := string(i)
 		size := uint64(65536)
-		subuid, _, err := subidAlloc.Alloc(size)
+		subuid, _, err := subidAlloc.Alloc(id, size)
 		if err != nil {
-			t.Errorf("AllocUid(%d) failed: %v", size, err)
+			t.Errorf("Alloc(%v, %v) failed: %v", id, size, err)
 			return
 		}
 		allocMap[subuid] = size
 	}
 
-	// reuse allocs
-	for i := 0; i < 2; i++ {
+	// re-alloc and verify ranges are re-used
+	for i := 2; i < 4; i++ {
+		id := string(i)
 		size := uint64(65536)
-		subuid, _, err := subidAlloc.Alloc(size)
+		subuid, _, err := subidAlloc.Alloc(id, size)
 		if err != nil {
-			t.Errorf("Alloc(%d) failed: %v", size, err)
+			t.Errorf("Alloc(%v, %v) failed: %v", id, size, err)
 			return
 		}
 		if _, ok := allocMap[subuid]; !ok {
-			t.Errorf("Alloc(%d) reuse failed: got %v", size, subuid)
+			t.Errorf("Alloc(%v, %v) reuse failed: got %v", id, size, subuid)
 			return
 		}
 	}
@@ -137,14 +140,35 @@ func TestAllocNoReuse(t *testing.T) {
 	}
 
 	var tests = []allocTest{
-		// size, wantUid, wantGid, wantErr
-		{1, 65536, 65536, ""},
-		{1, 131072, 131072, ""},
-		{1, 0, 0, "exhausted"},
+		// id, size, wantUid, wantGid, wantErr
+		{"1", 1, 65536, 65536, ""},
+		{"2", 1, 131072, 131072, ""},
+		{"3", 1, 0, 0, "exhausted"},
 	}
 
 	testAlloc(t, subidAlloc, tests)
 }
+
+func TestAllocErrors(t *testing.T) {
+
+	subuidCfg := strings.NewReader(`testUser:0:655360`) // range = 65530 * 10
+	subgidCfg := strings.NewReader(`testUser:0:655360`)
+
+	subidAlloc, err := New("testUser", NoReuse, subuidCfg, subgidCfg)
+	if err != nil {
+		t.Errorf("failed to create allocator: %v", err)
+		return
+	}
+
+	var tests = []allocTest{
+		// id, size, wantUid, wantGid, wantErr
+		{"1", 65536, 0, 0, ""},
+		{"1", 65536, 0, 0, "exhausted"},
+	}
+
+	testAlloc(t, subidAlloc, tests)	
+}
+
 
 func TestFreeBasic(t *testing.T) {
 	var err error
@@ -160,21 +184,21 @@ func TestFreeBasic(t *testing.T) {
 
 	// allocate until exhaustion
 	var tests = []allocTest{
-		{65536, 65536, 65536, ""},
-		{65536, 131072, 131072, ""},
-		{1, 0, 0, "exhausted"},
+		{"1", 65536, 65536, 65536, ""},
+		{"2", 65536, 131072, 131072, ""},
+		{"3", 1, 0, 0, "exhausted"},
 	}
 
 	testAlloc(t, subidAlloc, tests)
 
 	// free a range
-	if err := subidAlloc.Free(65536, 65536); err != nil {
+	if err := subidAlloc.Free("1"); err != nil {
 		t.Errorf("Free() returned %v; want no-err", err)
 	}
 
 	// reallocate to verify that free worked
 	tests = []allocTest{
-		{65536, 65536, 65536, ""},
+		{"1", 65536, 65536, 65536, ""},
 	}
 
 	testAlloc(t, subidAlloc, tests)
@@ -192,17 +216,17 @@ func TestFreeErrors(t *testing.T) {
 	}
 
 	var tests = []allocTest{
-		{65536, 0, 0, ""},
-		{1, 0, 0, "exhausted"},
+		{"1", 65536, 0, 0, ""},
+		{"2", 1, 0, 0, "exhausted"},
 	}
 
 	testAlloc(t, subidAlloc, tests)
 
-	if err := subidAlloc.Free(0, 0); err != nil {
+	if err := subidAlloc.Free("1"); err != nil {
 		t.Errorf("FreeUid() returned %v; want no-err", err)
 	}
 
-	if err := subidAlloc.Free(0, 0); err == nil {
+	if err := subidAlloc.Free("1"); err == nil {
 		t.Errorf("FreeUid() returned %v; want not-found", err)
 	}
 }
@@ -244,10 +268,11 @@ func TestConcurrency(t *testing.T) {
 	ch := make(chan allocResp, numWorkers)
 
 	for i := 0; i < numWorkers; i++ {
-		go func(subidAlloc intf.SubidAlloc, size uint64) {
-			uid, gid, err := subidAlloc.Alloc(size)
+		id := string(i)
+		go func(subidAlloc intf.SubidAlloc, id string, size uint64) {
+			uid, gid, err := subidAlloc.Alloc(id, size)
 			ch <- allocResp{uid, gid, err}
-		}(subidAlloc, allocSize)
+		}(subidAlloc, id, allocSize)
 	}
 
 	// wait for each goroutine to report back and verify they each got
@@ -273,14 +298,15 @@ func TestConcurrency(t *testing.T) {
 
 	ch2 := make(chan error, numWorkers)
 
-	for p := range subidMap {
-		go func(subidAlloc intf.SubidAlloc, uid, gid uint32) {
-			ch2 <- subidAlloc.Free(uid, gid)
-		}(subidAlloc, p.uid, p.gid)
+	for i := 0; i < numWorkers; i++ {
+		id := string(i)
+		go func(subidAlloc intf.SubidAlloc, id string) {
+			ch2 <- subidAlloc.Free(id)
+		}(subidAlloc, id)
 	}
 
 	// once all goroutines have done this, check that err = nil
-	for range subidMap {
+	for i := 0; i < numWorkers; i++ {
 		err := <-ch2
 		if err != nil {
 			t.Errorf("goroutine free failed: %v", err)
@@ -289,9 +315,10 @@ func TestConcurrency(t *testing.T) {
 
 	// reallocate to ensure freeing worked
 	for i := 0; i < numWorkers; i++ {
-		_, _, err := subidAlloc.Alloc(allocSize)
+		id := string(i)
+		_, _, err := subidAlloc.Alloc(id, allocSize)
 		if err != nil {
-			t.Errorf("AllocUid(%d) failed: %v", allocSize, err)
+			t.Errorf("AllocUid(%v, %v) failed: %v", id, allocSize, err)
 			return
 		}
 	}
@@ -309,9 +336,9 @@ func TestAllocLimits(t *testing.T) {
 	}
 
 	var tests = []allocTest{
-		// size, wantUid, wantGid, wantErr
-		{4294967296, 0, 0, ""},
-		{4294967296, 0, 0, "exhausted"},
+		// id, size, wantUid, wantGid, wantErr
+		{"1", 4294967296, 0, 0, ""},
+		{"2", 4294967296, 0, 0, "exhausted"},
 	}
 
 	testAlloc(t, subidAlloc, tests)
@@ -326,8 +353,8 @@ func TestAllocLimits(t *testing.T) {
 	}
 
 	tests = []allocTest{
-		// size, wantUid, wantGid, wantErr
-		{1, 0, 0, "exhausted"},
+		// id, size, wantUid, wantGid, wantErr
+		{"3", 1, 0, 0, "exhausted"},
 	}
 
 	testAlloc(t, subidAlloc, tests)
@@ -348,11 +375,12 @@ func TestAllocMultiRange(t *testing.T) {
 	}
 
 	var tests = []allocTest{
-		// size, wantUid, wantGid, wantErr
-		{65536, 0, 1048576, ""},
-		{65536, 524288, 1572864, ""},
-		{65536, 0, 0, "exhausted"},
+		// id, size, wantUid, wantGid, wantErr
+		{"1", 65536, 0, 1048576, ""},
+		{"2", 65536, 524288, 1572864, ""},
+		{"3", 65536, 0, 0, "exhausted"},
 	}
 
 	testAlloc(t, subidAlloc, tests)
 }
+