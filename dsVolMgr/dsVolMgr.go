7476465d068e0e9504e35264cc25ea734a24ed14
ctv10 <ctalledo@gmail.com>
2019-05-15 22:01:52 -0700

    First commit of sysvisor-mgr docker-store volume manager, which handles creation of a host directory that serves as a bind mount source for the sys container's "/var/lib/docker" directory.
    See sysvisor github issue #46 (https://github.com/nestybox/sysvisor/issues/46)
    for details.

---
+++ dsVolMgr/dsVolMgr.go
@@ -0,0 +1,163 @@
+// The docker-store volume manager creates and removes directories on the host that back
+// the sys container's Docker storage area (typically "/var/lib/docker"). This functionality
+// is needed in order to:
+//
+// * Remove the requirement for storing sysvisor system container images in a filesystem
+//   that supports Docker-in-Docker (e.g., btrfs).
+//
+// * Allow Docker-in-Docker when the outer docker (system container) is using uid-shifting
+//   (via the shiftfs module). This is needed because the inner Docker mounts overlayfs on
+//   top of the container images on `/var/lib/docker/`, but overlayfs does not work when
+//   mounted on top of shiftfs, so shiftfs can't be mounted on `/var/lib/docker`.
+//
+// See sysvisor github issue #46 (https://github.com/nestybox/sysvisor/issues/46) for
+// further details.
+
+package dsVolMgr
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"syscall"
+
+	"github.com/mrunalp/fileutils"
+	"github.com/nestybox/sysvisor/sysvisor-mgr/intf"
+	specs "github.com/opencontainers/runtime-spec/specs-go"
+	"github.com/sirupsen/logrus"
+	"golang.org/x/sys/unix"
+)
+
+// The docker storage directory can't be on the following filesystems, as docker inside
+// the sys container uses overlayfs for its images and overlayfs can't be mounted on top
+// of these.
+//
+// TODO: add shiftfs to the list, once we have the shiftfs magic defined.
+var unsupportedFs = map[string]int64{
+	"tmpfs":     unix.TMPFS_MAGIC,
+	"overlayfs": unix.OVERLAYFS_SUPER_MAGIC,
+}
+
+var checkUnsupportedFs = true // unit tests set this to false to ease testing
+
+type mgr struct {
+	hostDir string
+}
+
+// Creates a new instance of the docker-store volume manager.
+// 'hostDir' is the directory on the host which the manager will use for its operations
+func New(hostDir string) (intf.VolMgr, error) {
+	if checkUnsupportedFs {
+		var statfs syscall.Statfs_t
+		if err := syscall.Statfs(hostDir, &statfs); err != nil {
+			return nil, fmt.Errorf("failed to find filesystem info for %s", hostDir)
+		}
+		for name, magic := range unsupportedFs {
+			if statfs.Type == magic {
+				return nil, fmt.Errorf("host dir for docker store (%s) can't be on %v", hostDir, name)
+			}
+		}
+	}
+	logrus.Debugf("New docker store volume manager; host dir = %v", hostDir)
+	return &mgr{
+		hostDir: hostDir,
+	}, nil
+}
+
+// Implements intf.VolMgr.CreateVol
+func (dsm *mgr) CreateVol(id string, rootfs string, mountpoint string, uid, gid uint32, shiftUids bool) ([]specs.Mount, error) {
+	volPath := filepath.Join(dsm.hostDir, id)
+	mountPath := filepath.Join(rootfs, mountpoint)
+	mounts := []specs.Mount{}
+
+	if _, err := os.Stat(volPath); err == nil {
+		return mounts, fmt.Errorf("volume for container %v already exists", id)
+	}
+
+	if err := os.Mkdir(volPath, 0700); err != nil {
+		return mounts, fmt.Errorf("failed to create volume for container %v: %v", id, err)
+	}
+
+	// Set the ownership of the newly created volume to match the given uid(gid); this
+	// ensures that the container will have permission to access the volume. Note that by
+	// doing this, we also ensure that sysvisor-run won't mount shiftfs on top of the
+	// volume (which is important because Docker inside the sys container will mount
+	// overlayfs on this volume and overlayfs can't be mounted on top of shiftfs).
+	if err := os.Chown(volPath, int(uid), int(gid)); err != nil {
+		return mounts, fmt.Errorf("failed to set ownership of volume %v: %v", volPath, err)
+	}
+
+	// copy contents of container's mountpoint (if any) to the newly created volume
+	if _, err := os.Stat(mountPath); err == nil {
+		if err := fileutils.CopyDirectory(mountPath, volPath); err != nil {
+			os.RemoveAll(volPath)
+			return mounts, fmt.Errorf("failed to copy container's %v to host volume %v: %v", mountPath, volPath, err)
+		}
+	}
+
+	// If uid shifting is enabled in the sys container, manually shift the ownership of all
+	// copied files & directories (since shiftfs won't be mounted on this volume).
+	if shiftUids {
+		err := filepath.Walk(volPath, func(path string, fi os.FileInfo, err error) error {
+			if err == nil {
+				if path != volPath {
+					return shiftOwner(path, fi, uid, gid)
+				}
+			}
+			return err
+		})
+		if err != nil {
+			os.RemoveAll(volPath)
+			return mounts, fmt.Errorf("failed to shift ownership on host vol %v: %v", volPath, err)
+		}
+	}
+
+	m := specs.Mount{
+		Source:      volPath,
+		Destination: mountpoint,
+		Type:        "bind",
+		Options:     []string{"rbind", "rprivate"},
+	}
+	mounts = append(mounts, m)
+
+	logrus.Debugf("Created docker store volume at %v", volPath)
+	return mounts, nil
+}
+
+// Implements intf.VolMgr.DestroyVol
+func (dsm *mgr) DestroyVol(id string) error {
+	volPath := filepath.Join(dsm.hostDir, id)
+
+	if _, err := os.Stat(volPath); err != nil {
+		if os.IsNotExist(err) {
+			return nil
+		} else {
+			return fmt.Errorf("failed to stat %v: %v", volPath, err)
+		}
+	}
+
+	if err := os.RemoveAll(volPath); err != nil {
+		return fmt.Errorf("failed to remove %v: %v", volPath, err)
+	}
+
+	logrus.Debugf("Destroyed docker store volume at %v", volPath)
+	return nil
+}
+
+func shiftOwner(path string, fi os.FileInfo, uid, gid uint32) error {
+	st, ok := fi.Sys().(*syscall.Stat_t)
+	if !ok {
+		return fmt.Errorf("failed to stat %v", path)
+	}
+
+	// shift uid ownership from N to uid + N; same for gid
+	volUid := st.Uid + uid
+	volGid := st.Gid + gid
+
+	if err := os.Chown(path, int(volUid), int(volGid)); err != nil {
+		return fmt.Errorf("failed to change uid(gid) from %v:%v to %v:%v for %v: %v",
+			st.Uid, st.Gid, volUid, volGid, path, err)
+	}
+
+	return nil
+}
---
+++ dsVolMgr/dsVolMgr_test.go
@@ -0,0 +1,354 @@
+package dsVolMgr
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"syscall"
+	"testing"
+
+	specs "github.com/opencontainers/runtime-spec/specs-go"
+)
+
+func setupTest() (string, string, error) {
+	checkUnsupportedFs = false
+
+	hostDir, err := ioutil.TempDir("", "dsMountMgrTest-docker")
+	if err != nil {
+		return "", "", err
+	}
+
+	rootfs, err := ioutil.TempDir("", "dsMountMgrTest-rootfs")
+	if err != nil {
+		return "", "", err
+	}
+
+	return hostDir, rootfs, nil
+}
+
+func cleanupTest(hostDir, rootfs string) {
+	os.RemoveAll(hostDir)
+	os.RemoveAll(rootfs)
+}
+
+func populateDir(base string, files []string) error {
+	data := []byte("some data")
+
+	for _, file := range files {
+		dir := filepath.Dir(file)
+		path := filepath.Join(base, dir)
+		if err := os.MkdirAll(path, 0700); err != nil {
+			return fmt.Errorf("failed to create dir %v: %v", path, err)
+		}
+
+		path = filepath.Join(base, file)
+		if err := ioutil.WriteFile(path, data, 0700); err != nil {
+			return fmt.Errorf("failed to create file %v: %v", path, err)
+		}
+	}
+
+	return nil
+}
+
+func compareDirs(src, dest string) error {
+	var err error
+
+	srcPaths := []string{}
+	err = filepath.Walk(src, func(path string, fi os.FileInfo, err error) error {
+		if err == nil {
+			path = path[len(src):]
+			srcPaths = append(srcPaths, path)
+		}
+		return err
+	})
+
+	if err != nil {
+		return fmt.Errorf("failed walking path %v: %v", src, err)
+	}
+
+	destPaths := []string{}
+	err = filepath.Walk(dest, func(path string, fi os.FileInfo, err error) error {
+		if err == nil {
+			path = path[len(dest):]
+			destPaths = append(destPaths, path)
+		}
+		return err
+	})
+
+	if err != nil {
+		return fmt.Errorf("failed walking path %v: %v", dest, err)
+	}
+
+	if !equalStrings(srcPaths, destPaths) {
+		return fmt.Errorf("mismatch between %v and %v", srcPaths, destPaths)
+	}
+
+	return nil
+}
+
+func equalStrings(a, b []string) bool {
+	if len(a) != len(b) {
+		return false
+	}
+	for i := range a {
+		if a[i] != b[i] {
+			return false
+		}
+	}
+	return true
+}
+
+func equalMount(a, b specs.Mount) bool {
+	if a.Source != b.Source ||
+		a.Destination != b.Destination ||
+		a.Type != b.Type ||
+		!equalStrings(a.Options, b.Options) {
+		return false
+	}
+
+	return true
+}
+
+func equalMounts(a, b []specs.Mount) bool {
+	if len(a) != len(b) {
+		return false
+	}
+	for i := range a {
+		if !equalMount(a[i], b[i]) {
+			return false
+		}
+	}
+	return true
+}
+
+func testCreateVolWork(id, hostDir, rootfs, mountpoint string, uid, gid uint32, shiftUids bool) ([]specs.Mount, error) {
+	got := []specs.Mount{}
+
+	want := []specs.Mount{
+		{
+			Source:      filepath.Join(hostDir, id),
+			Destination: mountpoint,
+			Type:        "bind",
+			Options:     []string{"rbind", "rprivate"},
+		},
+	}
+
+	ds, err := New(hostDir)
+	if err != nil {
+		return got, fmt.Errorf("New(%v) returned %v", hostDir, err)
+	}
+
+	got, err = ds.CreateVol(id, rootfs, mountpoint, uid, gid, shiftUids)
+	if err != nil {
+		return got, fmt.Errorf("CreateVol() returned %v", err)
+	}
+
+	if !equalMounts(got, want) {
+		return got, fmt.Errorf("CreateVol(%v, %v, %v, %v, %v) returned %v, want %v", id, rootfs, mountpoint, uid, gid, got, want)
+	}
+
+	return got, nil
+}
+
+func TestCreateVol(t *testing.T) {
+	hostDir, rootfs, err := setupTest()
+	if err != nil {
+		t.Errorf("failed to setup test: %v", err)
+	}
+	defer cleanupTest(hostDir, rootfs)
+
+	id := "test-cont"
+	mountpoint := "/var/lib/docker"
+	uid := uint32(os.Geteuid())
+	gid := uint32(os.Getegid())
+
+	// create the volume and verify all is good
+	if _, err := testCreateVolWork(id, hostDir, rootfs, mountpoint, uid, gid, false); err != nil {
+		t.Errorf(err.Error())
+	}
+}
+
+func TestDestroyVol(t *testing.T) {
+
+	hostDir, rootfs, err := setupTest()
+	if err != nil {
+		t.Errorf("failed to setup test: %v", err)
+	}
+	defer cleanupTest(hostDir, rootfs)
+
+	ds, err := New(hostDir)
+	if err != nil {
+		t.Errorf("New(%v) returned %v", hostDir, err)
+	}
+
+	id := "test-cont"
+	mountpoint := "/var/lib/docker"
+	uid := uint32(os.Geteuid())
+	gid := uint32(os.Getegid())
+
+	_, err = ds.CreateVol(id, rootfs, mountpoint, uid, gid, false)
+	if err != nil {
+		t.Errorf("CreateVol() returned %v", err)
+	}
+
+	if err := ds.DestroyVol(id); err != nil {
+		t.Errorf("DestroyVol(%v) returned %v", id, err)
+	}
+
+	// Verify the volume was indeed destroyed
+	vol := filepath.Join(hostDir, id)
+	if _, err := os.Stat(vol); err != nil {
+		if !os.IsNotExist(err) {
+			t.Errorf("DestroyVol(%v) failed: %v", id, err)
+		}
+	}
+
+}
+
+func TestVolCopyUp(t *testing.T) {
+
+	hostDir, rootfs, err := setupTest()
+	if err != nil {
+		t.Errorf("failed to setup test: %v", err)
+	}
+	defer cleanupTest(hostDir, rootfs)
+
+	id := "test-cont"
+	mountpoint := "/var/lib/docker"
+	uid := uint32(os.Geteuid())
+	gid := uint32(os.Getegid())
+
+	files := []string{"testdir1/a/b/file0", "testdir2/c/d/file1", "testdir3/e/f/g/file2"}
+	baseDir := filepath.Join(rootfs, mountpoint)
+	if err := populateDir(baseDir, files); err != nil {
+		t.Errorf("failed to populate rootfs mountpoint: %v", err)
+	}
+
+	if _, err := testCreateVolWork(id, hostDir, rootfs, mountpoint, uid, gid, false); err != nil {
+		t.Errorf(err.Error())
+	}
+
+	src := filepath.Join(rootfs, mountpoint)
+	dest := filepath.Join(hostDir, id)
+	if err := compareDirs(src, dest); err != nil {
+		t.Errorf("directory comparison between %v and %v failed: %v", src, dest, err)
+	}
+}
+
+func TestVolOwnership(t *testing.T) {
+	uid := uint32(os.Geteuid())
+	gid := uint32(os.Getegid())
+
+	if uid != 0 && gid != 0 {
+		t.Skip("This test only runs as root, as it calls os.Chown()")
+	}
+
+	hostDir, rootfs, err := setupTest()
+	if err != nil {
+		t.Errorf("failed to setup test: %v", err)
+	}
+	defer cleanupTest(hostDir, rootfs)
+
+	id := "test-cont"
+	mountpoint := "/var/lib/docker"
+
+	mounts, err := testCreateVolWork(id, hostDir, rootfs, mountpoint, 1000, 1000, false)
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+
+	// Check that the created volume has correct ownership
+	vol := mounts[0].Source
+	fi, err := os.Stat(vol)
+	if err != nil {
+		t.Errorf("os.Stat(%v) returned %v", vol, err)
+	}
+	stat := fi.Sys().(*syscall.Stat_t)
+	if stat.Uid != 1000 || stat.Gid != 1000 {
+		t.Errorf("uid:gid mismatch on volume %v: want %v:%v, got %v:%v", vol, 1000, 1000, stat.Uid, stat.Gid)
+	}
+
+}
+
+func testUidShift(t *testing.T, shiftUids bool) {
+	uid := uint32(os.Geteuid())
+	gid := uint32(os.Getegid())
+
+	if uid != 0 && gid != 0 {
+		t.Skip("This test only runs as root, as it calls os.Chown()")
+	}
+
+	hostDir, rootfs, err := setupTest()
+	if err != nil {
+		t.Errorf("failed to setup test: %v", err)
+	}
+	defer cleanupTest(hostDir, rootfs)
+
+	// create a fake cont rootfs with contents in "/var/lib/docker"
+	id := "test-cont"
+	mountpoint := "/var/lib/docker"
+
+	files := []string{"testdir1/a/b/c/d/file0", "testdir1/a/file1", "testdir3/a/b/file2"}
+	baseDir := filepath.Join(rootfs, mountpoint)
+	if err := populateDir(baseDir, files); err != nil {
+		t.Errorf("failed to populate rootfs mountpoint: %v", err)
+	}
+
+	// set the ownerships on all files under "/var/lib/docker" to root:root
+	err = filepath.Walk(baseDir, func(path string, fi os.FileInfo, err error) error {
+		if err == nil {
+			if err := os.Chown(path, 0, 0); err != nil {
+				return err
+			}
+		}
+		return err
+	})
+
+	// create volume with 'shiftUids' set to true
+	uid = 231072
+	gid = 231072
+	_, err = testCreateVolWork(id, hostDir, rootfs, mountpoint, uid, gid, shiftUids)
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+
+	// verify that ownership of the volume and it's contents is set correctly
+	vol := filepath.Join(hostDir, id)
+	err = filepath.Walk(vol, func(path string, fi os.FileInfo, err error) error {
+		var (
+			wantUid uint32
+			wantGid uint32
+		)
+
+		// only the contents of the volume are subject to uid shifting; the volume itself
+		// always has the uid given to CreateVol()
+		if path == vol {
+			wantUid = 231072
+			wantGid = 231072
+		} else if shiftUids {
+			wantUid = uid
+			wantGid = gid
+		} else {
+			wantUid = 0
+			wantGid = 0
+		}
+
+		if err == nil {
+			stat := fi.Sys().(*syscall.Stat_t)
+			if stat.Uid != wantUid || stat.Gid != wantGid {
+				t.Errorf("uid:gid mismatch on volume path %v: want %v:%v, got %v:%v",
+					path, wantUid, wantGid, stat.Uid, stat.Gid)
+			}
+		}
+		return err
+	})
+}
+
+func TestUidShift(t *testing.T) {
+	testUidShift(t, true)
+}
+
+func TestNoUidShift(t *testing.T) {
+	testUidShift(t, false)
+}
