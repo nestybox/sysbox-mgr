72c42c08bddaa4d169ae1ddb0e480ba7a9b3fc8e
ctalledo <ctalledo@nestybox.com>
2019-12-04 03:19:54 +0000

    Back a sys container's /var/lib/kubelet with a sysbox-mgr volume manager.
    This change creates a sysbox-mgr volume manager to back
    the sys container's /var/lib/kubelet directory (similar
    to what sysbox does for the sys container's /var/lib/docker directory).
    
    This change is required because the K8s kubelet does not support
    shiftfs mounts over /var/lib/kubelet. See sysbox issue #472 for
    details.

--- volMgr/volMgr.go
+++ volMgr/volMgr.go
@@ -2,22 +2,19 @@
 // Copyright: (C) 2019 Nestybox Inc.  All rights reserved.
 //
 
-// The docker-store volume manager creates and removes directories on the host that are
-// mounted into the sys container's Docker storage area (typically
-// "/var/lib/docker"). This functionality is needed in order to:
+// The volume manager creates and removes a directory on the host that is bind-mounted
+// into the sys container, typically to overcome problems that arise if those directories
+// were to be on the sys container's rootfs (which typically uses overlayfs or
+// shiftfs-on-overlayfs mounts when uid shifting is enabled). The bind-mount overcomes
+// these problems since the source of the mount is a directory that is on the host's
+// filesystem (typically ext4).
 //
-// * Remove the requirement for storing sysbox system container images in a filesystem
-//   that supports Docker-in-Docker (e.g., btrfs).
-//
-// * Allow Docker-in-Docker when the outer docker is using uid-shifting
-//   (via the shiftfs module). This is needed because the inner Docker mounts overlayfs on
-//   top of the container images on `/var/lib/docker/`, but overlayfs does not work when
-//   mounted on top of shiftfs, so shiftfs can't be mounted on `/var/lib/docker`.
-//
-// See sysbox github issue #46 (https://github.com/nestybox/sysbox/issues/46) for
-// further details.
+// The volume manager takes care of ensuring that the newly created host directory
+// has correct ownership to allow sys container root processes to access it, and
+// also handles copying contents from the sys container to the bind-mount source
+// and vice-versa when the container is started, stopped, or paused.
 
-package dsVolMgr
+package volMgr
 
 import (
 	"bytes"
@@ -28,77 +25,52 @@ import (
 	"path/filepath"
 	"strconv"
 	"sync"
-	"syscall"
 
 	"github.com/nestybox/sysbox-mgr/intf"
 	specs "github.com/opencontainers/runtime-spec/specs-go"
 	"github.com/sirupsen/logrus"
-	"golang.org/x/sys/unix"
 )
 
-// The docker storage directory can't be on the following filesystems, as docker inside
-// the sys container uses overlayfs for its images and overlayfs can't be mounted on top
-// of these.
-const SHIFTFS_MAGIC int64 = 0x6a656a62
-
-var unsupportedFs = map[string]int64{
-	"tmpfs":     unix.TMPFS_MAGIC,
-	"overlayfs": unix.OVERLAYFS_SUPER_MAGIC,
-	"shiftfs":   SHIFTFS_MAGIC,
-}
-
-var checkUnsupportedFs = true // unit tests set this to false to ease testing
-
 type volInfo struct {
-	volPath   string // volume path in host
-	mountPath string // container path where volume is mounted
-	uid       uint32 // uid owner for container
-	gid       uint32 // gid owner for container
-	shiftUids bool   // uid(gid) shifting enabled for the volume
+	volPath   string      // volume path in host
+	mountPath string      // container path where volume is mounted
+	uid       uint32      // uid owner for container
+	gid       uint32      // gid owner for container
+	shiftUids bool        // uid(gid) shifting enabled for the volume
+	perm      os.FileMode // permissions for the volume
 }
 
-type mgr struct {
+type vmgr struct {
 	hostDir  string
 	volTable map[string]volInfo // cont id -> volume info
 	mu       sync.Mutex
 }
 
-// Creates a new instance of the docker-store volume manager.
+// Creates a new instance of the volume manager.
 // 'hostDir' is the directory on the host which the manager will use for its operations
 func New(hostDir string) (intf.VolMgr, error) {
-	if checkUnsupportedFs {
-		var statfs syscall.Statfs_t
-		if err := syscall.Statfs(hostDir, &statfs); err != nil {
-			return nil, fmt.Errorf("failed to find filesystem info for %s", hostDir)
-		}
-		for name, magic := range unsupportedFs {
-			if statfs.Type == magic {
-				return nil, fmt.Errorf("host dir for docker store (%s) can't be on %v", hostDir, name)
-			}
-		}
-	}
-	return &mgr{
+	return &vmgr{
 		hostDir:  hostDir,
 		volTable: make(map[string]volInfo),
 	}, nil
 }
 
 // Implements intf.VolMgr.CreateVol
-func (dsm *mgr) CreateVol(id, rootfs, mountpoint string, uid, gid uint32, shiftUids bool) (*specs.Mount, error) {
+func (m *vmgr) CreateVol(id, rootfs, mountpoint string, uid, gid uint32, shiftUids bool, perm os.FileMode) (specs.Mount, error) {
 	var err error
 
-	volPath := filepath.Join(dsm.hostDir, id)
+	volPath := filepath.Join(m.hostDir, id)
 	mountPath := filepath.Join(rootfs, mountpoint)
 
-	mount := &specs.Mount{}
+	mount := specs.Mount{}
 	if _, err = os.Stat(volPath); err == nil {
-		return mount, fmt.Errorf("volume directory for container %v already exists", id)
+		return mount, fmt.Errorf("volume dir for container %v already exists", id)
 	}
 
 	// create volume info
-	dsm.mu.Lock()
-	if _, found := dsm.volTable[id]; found {
-		dsm.mu.Unlock()
+	m.mu.Lock()
+	if _, found := m.volTable[id]; found {
+		m.mu.Unlock()
 		return mount, fmt.Errorf("volume for container %v already exists", id)
 	}
 	vi := volInfo{
@@ -107,27 +79,27 @@ func (dsm *mgr) CreateVol(id, rootfs, mountpoint string, uid, gid uint32, shiftU
 		uid:       uid,
 		gid:       gid,
 		shiftUids: shiftUids,
+		perm:      perm,
 	}
-	dsm.volTable[id] = vi
-	dsm.mu.Unlock()
+	m.volTable[id] = vi
+	m.mu.Unlock()
 
 	defer func() {
 		if err != nil {
-			dsm.mu.Lock()
-			delete(dsm.volTable, id)
-			dsm.mu.Unlock()
+			m.mu.Lock()
+			delete(m.volTable, id)
+			m.mu.Unlock()
 		}
 	}()
 
-	if err = os.Mkdir(volPath, 0700); err != nil {
+	if err = os.Mkdir(volPath, perm); err != nil {
 		return mount, fmt.Errorf("failed to create volume for container %v: %v", id, err)
 	}
 
 	// Set the ownership of the newly created volume to match the given uid(gid); this
 	// ensures that the container will have permission to access the volume. Note that by
 	// doing this, we also ensure that sysbox-runc won't mount shiftfs on top of the
-	// volume (which is important because Docker inside the sys container will mount
-	// overlayfs on this volume and overlayfs can't be mounted on top of shiftfs).
+	// volume.
 	if err = os.Chown(volPath, int(uid), int(gid)); err != nil {
 		os.RemoveAll(volPath)
 		return mount, fmt.Errorf("failed to set ownership of volume %v: %v", volPath, err)
@@ -135,34 +107,34 @@ func (dsm *mgr) CreateVol(id, rootfs, mountpoint string, uid, gid uint32, shiftU
 
 	// sync the contents of container's mountpoint (if any) to the newly created volume ("sync-in")
 	if _, err := os.Stat(mountPath); err == nil {
-		if err = dsm.rsyncVol(mountPath, volPath, uid, gid, shiftUids); err != nil {
+		if err = m.rsyncVol(mountPath, volPath, uid, gid, shiftUids); err != nil {
 			os.RemoveAll(volPath)
 			return mount, fmt.Errorf("volume sync-in failed: %v", err)
 		}
 	}
 
-	mount = &specs.Mount{
+	mount = specs.Mount{
 		Source:      volPath,
 		Destination: mountpoint,
 		Type:        "bind",
 		Options:     []string{"rbind", "rprivate"},
 	}
 
-	logrus.Debugf("Created docker store volume at %v", volPath)
+	logrus.Debugf("Created volume at %v", volPath)
 	return mount, nil
 }
 
 // Implements intf.VolMgr.DestroyVol
-func (dsm *mgr) DestroyVol(id string) error {
+func (m *vmgr) DestroyVol(id string) error {
 
-	dsm.mu.Lock()
-	vi, found := dsm.volTable[id]
+	m.mu.Lock()
+	vi, found := m.volTable[id]
 	if !found {
-		dsm.mu.Unlock()
+		m.mu.Unlock()
 		return fmt.Errorf("failed to find vol info for container %s", id)
 	}
 	volPath := vi.volPath
-	dsm.mu.Unlock()
+	m.mu.Unlock()
 
 	if _, err := os.Stat(volPath); err != nil {
 		return fmt.Errorf("failed to stat %v: %v", volPath, err)
@@ -172,24 +144,24 @@ func (dsm *mgr) DestroyVol(id string) error {
 		return fmt.Errorf("failed to remove %v: %v", volPath, err)
 	}
 
-	dsm.mu.Lock()
-	delete(dsm.volTable, id)
-	dsm.mu.Unlock()
+	m.mu.Lock()
+	delete(m.volTable, id)
+	m.mu.Unlock()
 
-	logrus.Debugf("Destroyed docker store volume at %v", volPath)
+	logrus.Debugf("Destroyed volume at %v", volPath)
 	return nil
 }
 
 // Implements intf.VolMgr.SyncOut
-func (dsm *mgr) SyncOut(id string) error {
+func (m *vmgr) SyncOut(id string) error {
 
-	dsm.mu.Lock()
-	vi, found := dsm.volTable[id]
+	m.mu.Lock()
+	vi, found := m.volTable[id]
 	if !found {
-		dsm.mu.Unlock()
+		m.mu.Unlock()
 		return fmt.Errorf("failed to find vol info for container %s", id)
 	}
-	dsm.mu.Unlock()
+	m.mu.Unlock()
 
 	// mountPath is the sync-out target; if it does not exist, create it (but only if we
 	// are going to be copying anything to it).
@@ -199,7 +171,7 @@ func (dsm *mgr) SyncOut(id string) error {
 			return fmt.Errorf("error while checking if %s is empty: %s", vi.volPath, err)
 		}
 		if !volIsEmpty {
-			if err := os.MkdirAll(vi.mountPath, 0700); err != nil {
+			if err := os.MkdirAll(vi.mountPath, vi.perm); err != nil {
 				return fmt.Errorf("failed to create directory %s: %s", vi.mountPath, err)
 			}
 		}
@@ -207,7 +179,7 @@ func (dsm *mgr) SyncOut(id string) error {
 
 	// if the sync-out target exists, perform the rsync
 	if _, err := os.Stat(vi.mountPath); err == nil {
-		if err := dsm.rsyncVol(vi.volPath, vi.mountPath, 0, 0, vi.shiftUids); err != nil {
+		if err := m.rsyncVol(vi.volPath, vi.mountPath, 0, 0, vi.shiftUids); err != nil {
 			return fmt.Errorf("volume sync-out failed: %v", err)
 		}
 	}
@@ -217,7 +189,7 @@ func (dsm *mgr) SyncOut(id string) error {
 
 // rsyncVol performs an rsync from src to dest; if shiftUids is true, the rsync
 // modifies the ownership of files copied to dest to match the givne uid(gid).
-func (dsm *mgr) rsyncVol(src, dest string, uid, gid uint32, shiftUids bool) error {
+func (m *vmgr) rsyncVol(src, dest string, uid, gid uint32, shiftUids bool) error {
 
 	var cmd *exec.Cmd
 	var stdout, stderr bytes.Buffer
@@ -225,11 +197,10 @@ func (dsm *mgr) rsyncVol(src, dest string, uid, gid uint32, shiftUids bool) erro
 	srcDir := src + "/"
 
 	// Note: rsync uses file modification time and size to determine if a sync is
-	// needed. This should be fine for sync'ing the sys container's /var/lib/docker given
-	// the way it's used internally by Docker (where the probability of files being
-	// different yet having the same size & timestamp is low). If this assumption changes
-	// we could pass the `--checksum` option to rsync, but this will slow the copy
-	// operation significantly.
+	// needed. This should be fine for sync'ing the sys container's directories given,
+	// assuming the probability of files being different yet having the same size &
+	// timestamp is low. If this assumption changes we could pass the `--checksum` option
+	// to rsync, but this will slow the copy operation significantly.
 
 	if shiftUids {
 		chown := "--chown=" + strconv.FormatUint(uint64(uid), 10) + ":" + strconv.FormatUint(uint64(gid), 10)
